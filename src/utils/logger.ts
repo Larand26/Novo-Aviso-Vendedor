b9aa95db60bb24cec8f14acf5eee78468a341c12
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-04-17 13:44:49 -0300

    refactor: altera o modo do chalk

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,11 @@
 import chalk from "chalk";
 
 export const logger = {
-  info: (message: string) => console.log(chalk.blue(message)),
-  success: (message: string) => console.log(chalk.green(message)),
-  warning: (message: string) => console.log(chalk.yellow(message)),
-  error: (message: string) => console.error(chalk.red(message)),
+  info: (message: string) => console.log(chalk.blue.bold("[Info]"), message),
+  success: (message: string) =>
+    console.log(chalk.green.bold("[Success]"), message),
+  warning: (message: string) =>
+    console.log(chalk.yellow.bold("[Warning]"), message),
+  error: (message: string) =>
+    console.error(chalk.red.bold.underline("[Error]"), message),
 };
