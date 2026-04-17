91ab01a030339f10da5d0ea4c890ac6a232aef62
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-04-17 13:37:49 -0300

    feat: ajusta logs usando o chalk para melhor visualização

--- src/controllers/ClientsController.ts
+++ src/controllers/ClientsController.ts
@@ -1,7 +1,11 @@
 import ClientsService from "../services/ClientsService.js";
 
 abstract class ClientsController {
-  static async getClientsDattaFromApi(): Promise<Object> {
+  static async getClientsDattaFromApi(): Promise<{
+    success: boolean;
+    data?: Array<object>;
+    error?: string;
+  }> {
     return await ClientsService.getClientsDataFromApi();
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import ClientsController from "./controllers/ClientsController.js";
+import { logger } from "./utils/Logger.js";
 
 const main = async (): Promise<void> => {
   try {
     // Pega os dados dos clientes na api
     const clientsData = await ClientsController.getClientsDattaFromApi();
-    console.log(clientsData);
+
+    if (!clientsData || clientsData.success === false) {
+      logger.error(
+        "Falha ao obter os dados dos clientes: " + clientsData.error,
+      );
+    }
+
+    logger.success("Dados dos clientes obtidos com sucesso!");
   } catch (error) {
-    console.error("An error occurred:", error);
+    logger.error("An error occurred: " + error);
   }
 };
 main();
