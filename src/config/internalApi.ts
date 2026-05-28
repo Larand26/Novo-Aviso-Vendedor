4b3b989b569475b7afac0a315a2129d75d5b96b3
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-05-28 10:55:04 -0300

    fix: puxa o api token para config

--- src/config/internalApi.ts
+++ src/config/internalApi.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const configs = {
   apiUrl: process.env.URL_INTERNAL_API || "http://localhost:3000",
   timeout: 5000, // Tempo limite para requisições em milissegundos
-  apiToken: process.env.INTERNAL_API || "", // Token de autenticação para a API interna
+  apiToken: process.env.INTERNAL_API_TOKEN || "", // Token de autenticação para a API interna
 };
 
 export default configs;
